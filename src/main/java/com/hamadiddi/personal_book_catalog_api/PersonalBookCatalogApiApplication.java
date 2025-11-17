8fe3b763b7c3305e8e2227ff3aadd6ce7ec56930
Hamadi Iddi <hamaditaufik@gmail.com>
2025-11-17 09:51:54 +0300

    Initial commit

---
+++ src/main/java/com/hamadiddi/personal_book_catalog_api/PersonalBookCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.hamadiddi.personal_book_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBookCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBookCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamadiddi/personal_book_catalog_api/PersonalBookCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamadiddi.personal_book_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBookCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
